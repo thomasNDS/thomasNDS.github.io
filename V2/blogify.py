39aaf358a5bd6acc5d7397a41ce613a4f0677ade
thomas <nds.thomas@gmail.com>
2014-04-08 22:05:42 +0200

    minification autmoatique

--- V2/blogify.py
+++ V2/blogify.py
@@ -5,26 +5,43 @@
 #
 # copyright Thomas Nunes <thomasnds.github.io>
 # License MIT
+#########################################################################
 
 import os
 import sys
 from package import *
 import cssmin
 
-#need arguments ?
-if len(sys.argv) > 2:
-    print sys.argv[1],sys.argv[2],sys.argv[3],sys.argv[4]
-    city = sys.argv[1]
-    argument = sys.argv[2]
+############################-Minification-###############################
+def minifyFiles():
+    # Minify all files
+    for file in file2min:
+        if file != "index.html":
+            output = file.replace(".","-min.")
+        os.system("./bin/jsmin <" + file + " >" + output)
 
-#Files imported
-doctype = (open("templates/doctype.html", "r")).read()
-headerHtml = (open("templates/header.html", "r")).read()
-importJSHtml = (open("templates/importsJS.html", "r")).read()
-importCSSHtml = (open("templates/importsCSS.html", "r")).read()
-footerHtml = (open("templates/footer.html", "r")).read()
+    # Merge all css files
+    cssConcat = ""
+    for css in css2min:
+        cssConcat += cssmin.cssmin(open(css, "r").read())
 
-#page object
+    open(path2cssMin, "w").write(cssConcat)
+
+    # Merge all js files
+    jsConcat = ""
+    for js in js2min:
+        js = js.replace(".","-min.")
+        jsConcat += open(js, "r").read()
+    open(path2jsMin, "w").write(jsConcat)
+    print "all files minified !"
+
+#########################################################################
+# Page object
+#
+# -path : (string) path of the new page
+# -title : the tile of the page <title>
+# -descr : the description of the page <meta> 
+#########################################################################
 class Page:
     html =  ""
     def __init__(self, path, title, descr):
@@ -36,41 +53,61 @@ class Page:
         self.write( self.desc + '">')
         self.write("<title>"+ self.title + "</title>")
         self.write(importJSHtml + importCSSHtml+ "</head><body><div id='wrap'>")
-        self.write(headerHtml)
+        self.write('<div id="main">')
         
     def write(self,content):
         self.file.write(content)
     
     def close(self):
-        self.write(footerHtml)
         self.file.close()
         print self.path, "page saved !"
+        
+#########################################################################
+# Page section object like a header, footer or contact form (html)
+#
+# -path : (string) the path of the html file 
+#########################################################################
+class PageSection:
+    def __init__(self,path):
+        self.path = path
+        self._file = open(path, "r")
+        self.content = (self._file).read()
+        self._file.close()
+        
+    def getHtml(self):
+        return self.content
 
-#Create a start page (index)
+############################-Create pages-###############################
+
+#need arguments ?
+if len(sys.argv) > 2:
+    print sys.argv[1],sys.argv[2],sys.argv[3],sys.argv[4]
+    city = sys.argv[1]
+    argument = sys.argv[2]
+
+# Files imported
+doctype = (open("templates/doctype.html", "r")).read()
+importJSHtml = (open("templates/importsJS.html", "r")).read()
+importCSSHtml = (open("templates/importsCSS.html", "r")).read()
+
+
+
+
+# Create a start page (index)
 indexHtml = Page('indexbis.html',"Thomas Nunes website","Personnal website of Thomas Nunes. thomasNDS")
+headerComponent = PageSection("pages/components/header.html")
+indexHtml.write(headerComponent.getHtml())
+
 indexHtml.write("hello world ! ")
+contactComponent = PageSection("pages/components/contact.html")
+indexHtml.write(contactComponent.getHtml())
+
+footerComponent = PageSection("pages/components/footer.html")
+indexHtml.write(footerComponent.getHtml())
+
 indexHtml.close()
 
-#minify all files
-for file in file2min:
-    if file != "index.html":
-        output = file.replace(".","-min.")
-    os.system("./bin/jsmin <" + file + " >" + output)
+minifyFiles()
 
-# merge all css files
-cssConcat = ""
-for css in css2min:
-    cssConcat += cssmin.cssmin(open(css, "r").read())
-    
-open(path2cssMin, "w").write(cssConcat)
-
-# merge all js files
-jsConcat = ""
-for js in js2min:
-    js = js.replace(".","-min.")
-    jsConcat += open(js, "r").read()
-    
-open(path2jsMin, "w").write(jsConcat)
-
-#Delete file generated
+# Delete file generated
 os.system("rm indexbis.html")
--- V2/package.py
+++ V2/package.py
@@ -1,5 +1,13 @@
+#! /usr/bin/python2.7
+# -*-coding:Utf-8 -*
+#
+# Static blog generator
+#
+# copyright Thomas Nunes <thomasnds.github.io>
+# License MIT
+
 css2min = [
-            "public/css/libs/bootstrap.min.css",
+            "public/css/libs/bootstrap.css",
             "public/css/libs/bootflat.css",
             "public/css/src/general.css",
             ]
@@ -9,11 +17,14 @@ js2min = [
             "public/js/libs/respond.min.js",
             "public/js/libs/icheck.min.js",
             "public/js/src/stellarConfig.js",
+            "public/js/src/scrollButton.js",
             ]
 html2min = [
             "indexbis.html",
             ]
+            
 file2min = js2min + html2min
 
+#Where generated files will be set
 path2cssMin = "public/gen/min.css"
 path2jsMin = "public/gen/min.js"
