fcc128881cd00d2c67486c493f3daceebb829eee
thomas <nds.thomas@gmail.com>
2015-01-19 22:18:17 +0100

    dist add

---
+++ v2/dist/font/README.md
@@ -0,0 +1,1 @@
+http://socicon.com/
---
+++ v2/dist/scripts/6c579fb9.main.js
@@ -0,0 +1,1 @@
+"use strict";function MainCtrl(){this.someObject="azerty",this.doSomething=function(){}}angular.module("MyPage",["ngRoute"]).controller("MainCtrl",MainCtrl).config(["$routeProvider",function(a){a.when("/",{templateUrl:"views/main.html"}).when("/about",{templateUrl:"views/about.html"}).otherwise({redirectTo:"/"})}]);
