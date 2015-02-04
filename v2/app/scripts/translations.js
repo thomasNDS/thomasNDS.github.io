b7ea9ac76a99cad8cbd70e9d48aeb539ff936325
thomas <nds.thomas@gmail.com>
2015-02-04 12:41:15 +0100

    change colors and corest mistake

--- v2/app/scripts/translations.js
+++ v2/app/scripts/translations.js
@@ -12,25 +12,25 @@ var translationsEN = {
     iae: 'Grenoble IAE',
     availableToWork: 'I will be available to work in May 2015',
     contactMe: 'Please contact me throw socials networks.',
-    webOnMit: 'This website are Created by myself, and is totaly open-source under MIT licence',
+    webOnMit: 'This website was created by myself, and is totaly open-source, under MIT licence',
     codeIsOn: 'Code is available on',
-    technologiesUsed: 'Technologies used:',
+    technologiesUsed: 'Technologies used on this website:',
     DescrAngular: 'javascript MVC framework, to structure the website',
     descrMaterialize: 'CSS framework based on ',
     descrLess: 'a dynamic stylesheet language which compile to css',
     colorsFrom: 'Colors from ',
     palette: 'palette',
-    subtitle: 'Web and mobile developer'
+    subtitle: 'Web and mobile development engineer'
 }
 
 var translationsFR = {
     en : 'fr',
     EN : 'FR',
-    frenchEngineer: 'Je suis un ingénieur en développement informatique français',
-    myIntership: 'Je viens de terminer mon stage de fin d\'étude à',
-    in: ' au sein de ',
-    softwareEngineer: ' en tant qu\'ingénieur en développement travaillant dans le domaine de processus métiers (Business Process Management).',
-    iStudy: 'J\'étude actuelement le management des entreprises à ',
+    frenchEngineer: 'Je suis un ingénieur en développement informatique français.',
+    myIntership: 'Je viens de terminer mon stage de fin d\'étude  au sein de ',
+    in: ' à ',
+    softwareEngineer: ' en tant qu\'ingénieur en développement, travaillant dans le domaine de processus métiers (Business Process Management).',
+    iStudy: 'J\'étudie actuellement le management des entreprises à ',
     iae: 'l\'IAE de Grenoble',
     availableToWork: 'Je serais disponible pour toutes opportunités professionnelles à partir de mai 2015',
     contactMe: 'N\'hésitez pas à me contacter à travers les différents réseaux sociaux où je suis présent.',
@@ -38,11 +38,11 @@ var translationsFR = {
     About: 'À-propos',
     webOnMit: 'Ce site internet a été crée par moi-même. L\'intégralité du site est open-source sous licence MIT',
     codeIsOn: 'Le code source est disponible sur ',
-    technologiesUsed: 'Technologies utilisées :',
+    technologiesUsed: 'Technologies utilisées sur ce site :',
     DescrAngular: '  un framework  javascript MVC utilisé pour structurer le site',
     descrMaterialize: ' un framework CSS basé sur ',
     descrLess: ' un langage dynamique de génération de feuilles de style compilable en css.',
     colorsFrom: 'Palette de couleurs ',
     palette: ' a',
-    subtitle: 'Développeur web et mobile'
+    subtitle: 'Ingénieur en développement web et mobile'
 }
