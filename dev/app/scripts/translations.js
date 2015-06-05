fc99af746cba9294dfc1a4af9d63ef272fb7aaed
thomas <nds.thomas AT gmail.com>
2015-06-05 21:18:48 +0200

    V 0.0.4

--- dev/app/scripts/translations.js
+++ dev/app/scripts/translations.js
@@ -21,6 +21,7 @@ var translationsEN = {
     subtitle: 'Web and mobile development engineer',
     moreOnWikipedia: 'More on Wikipedia',
     descrNorsys: 'Norsys is a french IT services company',
+    moreInfos: 'more about',
     moreNorsys: 'http://fr.wikipedia.org/wiki/Norsys'
 }
 
@@ -28,21 +29,23 @@ var translationsFR = {
     en : 'fr',
     EN : 'FR',
     frenchEngineer: 'Je suis ingénieur en développement informatique.',
-   myjob: 'I currently work at',
-    norsys: 'Norsys company',
-    onfrench: 'on french national education projects, as a web and mobile developer.',
+    myjob: 'Je travaille actuelement au sein de',
+    norsys: 'la société Norsys',
+    onfrench: 'sur des projets pour l\'éducation nationale, en tant que développeur web et mobile.',
     contactMe: 'N\'hésitez pas à me contacter à travers les différents réseaux sociaux où je suis présent.',
     Home: 'Accueil',
     About: 'À-propos',
-    webOnMit: 'Ce site internet a été crée par moi-même. L\'intégralité du site est open-source sous licence MIT',
-    mit: "MIT licence",
+    webOnMit: 'J\'ai personnellement ce site dont l\'intégralité du site est open-source sous',
+    mit: "licence MIT",
     codeIsOn: 'Le code source est disponible sur ',
     technologiesUsed: 'Technologies utilisées sur ce site :',
     DescrAngular: '  un framework  javascript MVC utilisé pour structurer le site',
     descrMaterialize: ' un framework CSS basé sur ',
     descrLess: ' un langage dynamique de génération de feuilles de style compilable en css.',
-    colorsFrom: 'Palette de couleurs ',
+    colorsFrom: 'La palette de couleurs ',
     palette: ' a',
     subtitle: 'Ingénieur en développement web et mobile',
+    return: 'Retour',
+    moreInfos: 'Plus d\'info',
     moreOnWikipedia: 'Plus d\'infos sur Wikipedia'
 }
